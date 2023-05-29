b27beff96ae85e5f4ae07fcd9624f36c0873517d
DT0072 <dtkelvin9@gmail.com>
2023-05-29 23:03:40 +0800

    [Capstone] Admin Dashboard Changes

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -6,13 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-dashboard.component.css']
 })
 export class AdminDashboardComponent {
-  isAdminLoggedIn: boolean = true;
-
-  // Function to check if admin login is successful
-  checkAdminLogin() {
-    // Perform your logic to check if the admin login is successful
-    // Set the value of `isAdminLoggedIn` accordingly
-    // For example:
-    this.isAdminLoggedIn = true;
-  }
+  
 }
