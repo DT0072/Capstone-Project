5735a71255b436228c1048d47f2f6faea9fd4de3
DT0072 <dtkelvin9@gmail.com>
2023-06-04 03:18:39 +0800

    [Capstone] Admin Dashboard Changes

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-dashboard.component.css']
 })
 export class AdminDashboardComponent {
-  
+  isAdmin: boolean = false;
+
+  constructor(private afAuth: AngularFireAuth){}
+
+  ngOnInit(): void {
+    this.checkAdminLogin();
+  }
+
+  checkAdminLogin(): void {
+    this.afAuth.authState.subscribe(user => {
+      if(user && user.uid === 'kwoY1TF3tpdhtHQnEZjU5TpOFz93'){
+        this.isAdmin = true;
+      }else {
+        this.isAdmin = false;
+      }
+    });
+  }
 }
