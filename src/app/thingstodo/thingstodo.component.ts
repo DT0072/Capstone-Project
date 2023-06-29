8819bb1eaba43c5b0b1f80c289ad6ce512e009ae
XY <p21013467@student.newinti.edu.my>
2023-06-30 01:46:57 +0800

    Things to do & UI Alignment

--- src/app/thingstodo/thingstodo.component.ts
+++ src/app/thingstodo/thingstodo.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AttData } from '../model/att-data';
+import { DataService } from '../shared/data.service';
 
 @Component({
   selector: 'app-thingstodo',
   templateUrl: './thingstodo.component.html',
   styleUrls: ['./thingstodo.component.css']
 })
-export class ThingstodoComponent {
+export class ThingstodoComponent implements OnInit {
+  attdataList: AttData[] = [];
+  displayData: AttData[] = [];
+ 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private dataService: DataService
+  ) {}
 
+  ngOnInit(): void {
+    this.getAllAttractions();
+  }
+
+  getAllAttractions() {
+    this.dataService.getAllAttractions().subscribe(
+      (res: any) => {
+        this.attdataList = res.map((e: any) => {
+          const data = e.payload.doc.data();
+          data.att_id = e.payload.doc.id;
+          return data;
+        });
+  
+        this.displayData = this.attdataList.filter((attData: AttData) => {
+          return attData.att_price != '-';
+        });
+  
+        this.sortData(); 
+      },
+      (err: any) => {
+        alert('Error while fetching attractions, please try again later');
+      }
+    );
+  }
+  
+
+  formatTime(time: string): string {
+    if (time === '24 Hours') {
+      return time;
+    } else if (time === '-' || !time) {
+      return '';
+    } else {
+      const date = new Date(`2000-01-01T${time}`);
+      const formattedTime = date.toLocaleTimeString([], {
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true,
+        hourCycle: 'h23'
+      });
+      return formattedTime;
+    }
+  }
+
+  sortData() {
+    this.displayData.sort((a, b) => {
+      return a.att_name.localeCompare(b.att_name);
+    });
+  }
+  
 }
