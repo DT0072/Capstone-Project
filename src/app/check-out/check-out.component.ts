f5721dfe406608f210387d96ef05a34ecff5a3e6
XY <p21013467@student.newinti.edu.my>
2023-07-05 21:37:30 +0800

    check out

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -13,40 +13,44 @@ export class CheckOutComponent implements OnInit {
   selectedData: any;
   progress: number = 0;
   visibleForms: string[] = [];
+  public products: any = [];
+  grandTotal: number = 0;
+  fromBookingPage: boolean = true; // Assuming default source is booking page
 
   constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
     this.selectedData = history.state;
-  console.log('Selected Data:', this.selectedData);
+    console.log('Selected Data:', this.selectedData);
 
-  const { att_openHrs, att_closeHrs } = this.selectedData;
-  console.log('att_openHrs:', att_openHrs);
-  console.log('att_closeHrs:', att_closeHrs);
-  
-  console.log('Time parameter:', this.formatTime(att_openHrs));
-  console.log('Time parameter:', this.formatTime(att_closeHrs));
-  // this.selectedData.forEach((a: any) => {
-  //   Object.assign(a,{quantity:1,total:a.price});
-  // });
+    const { att_openHrs, att_closeHrs } = this.selectedData;
+    console.log('att_openHrs:', att_openHrs);
+    console.log('att_closeHrs:', att_closeHrs);
 
-  this.visibleForms = ['card-form'];
+    console.log('Time parameter:', this.formatTime(att_openHrs));
+    console.log('Time parameter:', this.formatTime(att_closeHrs));
+
+    // Determine the source (booking or cart)
+    this.fromBookingPage = this.selectedData.fromBookingPage ?? true;
+
+    this.visibleForms = ['card-form'];
+
   }
 
   formatTime(time: string): string {
     console.log('Time parameter:', time);
-  
+
     if (time === '24 Hours') {
       return time; // Return "24 Hours" as is
     }
-  
+
     if (time === '-' || !time) {
       return ''; // Return an empty string or handle the case when time is '-' or undefined
     }
-  
+
     // Convert the time string to a JavaScript Date object
     const date = new Date(`2000-01-01T${time}`);
-  
+
     // Format the time using options for hour12 and hourCycle
     const formattedTime = date.toLocaleTimeString([], {
       hour: 'numeric',
@@ -54,7 +58,7 @@ export class CheckOutComponent implements OnInit {
       hour12: true,
       hourCycle: 'h23'
     });
-  
+
     return formattedTime; // Return the formatted time without the label
   }
 
@@ -73,4 +77,5 @@ export class CheckOutComponent implements OnInit {
   isFormVisible(formId: string): boolean {
     return this.visibleForms.includes(formId);
   }
+
 }
